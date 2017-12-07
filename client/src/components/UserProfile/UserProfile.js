82452c83e999a9e3e13def32a29001db749f8c11
Benjamin Malin <benjaminamalin@gmail.com>
2017-12-07 12:26:30 -0500

    User Profile displays basic user information

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -6,6 +6,10 @@ export default class UserProfile extends Component {
   render() {
     return (
       <div>
+        <img src="https://www.facebook.com/images/fb_icon_325x325.png"></img>
+        <h3>Facebook</h3>
+        <h4>Menlo Park, CA</h4>
+        <p>We melt brains, one notification at a time.</p>
       </div>
     )
   }
--- client/src/components/UserProfile/UserProfile.test.js
+++ client/src/components/UserProfile/UserProfile.test.js
@@ -7,4 +7,13 @@ describe('Core display functionality of User Profile', () => {
   it('Smoke test - component should render', () => {
     shallow(< UserProfile />);
   });
-})
+  it('Should display basic user information- logo/avatar, name, location, description', () => {
+    const wrapper = shallow(<UserProfile />);
+    expect(wrapper.containsAllMatchingElements([
+      <img src="https://www.facebook.com/images/fb_icon_325x325.png"></img>,
+      <h3>Facebook</h3>,
+      <h4>Menlo Park, CA</h4>,
+      <p>We melt brains, one notification at a time.</p>
+    ])).toEqual(true);
+  });
+});
