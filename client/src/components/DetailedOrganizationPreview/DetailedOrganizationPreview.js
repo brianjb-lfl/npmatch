ae74adf3855bdc42d6a0be19a11d47e70521e9e3
Benjamin Malin <benjaminamalin@gmail.com>
2017-12-07 09:58:36 -0500

    Tested Detailed Org Preview to render the correct props

--- client/src/components/DetailedOrganizationPreview/DetailedOrganizationPreview.js
+++ client/src/components/DetailedOrganizationPreview/DetailedOrganizationPreview.js
@@ -6,6 +6,9 @@ export default class DetailedOrganizationPreview extends Component {
   render() {
     return (
       <div>
+        <img src="https://www.facebook.com/images/fb_icon_325x325.png"></img>,
+        <h3>Facebook</h3>,
+        <p>We melt brains, one notification at a time.</p>
       </div>
     )
   }
--- client/src/components/DetailedOrganizationPreview/DetailedOrganizationPreview.test.js
+++ client/src/components/DetailedOrganizationPreview/DetailedOrganizationPreview.test.js
@@ -7,4 +7,16 @@ describe('Detailed Organization Preview component display functionality', () => 
   it('Smome test - component should render', () => {
     shallow(<DetailedOrganizationPreview />);
   });
+  it('Should use the props for logo, name, and description', () => {
+    const wrapper = shallow(<DetailedOrganizationPreview 
+      logo="https://www.facebook.com/images/fb_icon_325x325.png"
+      name="Facebook"
+      description="We melt brains, one notification at a time."
+    />);
+    expect(wrapper.containsAllMatchingElements([
+      <img src="https://www.facebook.com/images/fb_icon_325x325.png"></img>,
+      <h3>Facebook</h3>,
+      <p>We melt brains, one notification at a time.</p>
+    ])).toEqual(true);
+  })
 })
