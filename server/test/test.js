088d3d9a7362ce852c889a8156e10905f21ff804
Brian Besaw <bjbesaw@outlook.com>
2017-12-08 21:40:26 -0500

    Updated test setup in preparation for detailed user GET testing.

--- server/test/test.js
+++ server/test/test.js
@@ -37,46 +37,47 @@ describe('user', function() {
   });
 
   // ***** GET USER LIST
-  describe('api/user/list GET user list', function() {
+  describe('api/users/list GET user list', function() {
 
     it('should return a list of existing users', function() {
       return testData.seedUserTable()
 
         .then( () => {
           return chai.request(app)
-            .get('/api/user/list')
+            .get('/api/users/list')
             .then(function(res) {
               let userNames = testData.userSeeds.map( item => item.username );
-              expect(res.body.length).to.equal(2);
-              expect(userNames).to.include(res.body[0].username);
-              expect(userNames).to.include(res.body[1].username);
+              expect(res.body.length).to.equal(testData.userSeeds.length);
+              for(let uCtr = 0; uCtr < testData.userSeeds.length; uCtr++) {
+                expect(userNames).to.include(res.body[uCtr].username);
+              }
             });
         });
     });
   });
 
   // ***** GET INDIVIDUAL USER - DETAIL
-  describe('api/user/:id GET user details', function() {
+  // describe('api/user/:id GET user details', function() {
     
   
   
-    // base user info
-    // array of links - links, id_user = id
-    // array of causes - users_causes - id_user = id, id_cause = causes id
-    // array of skills - users_skills - id_user = id,  id_skill = skills id 
-    // array of responses - responses, id_user = id, id_opp = opportunities id, id_user = orgs id
-    // array of adminOf - roles - id_user_receiving = id, id_user_adding = id of org where capabilities = admin
-    // array of followingroles - id_user_receiving = id, id_user_adding = id of org where capabilities = admin
-  });
+  //   // base user info
+  //   // array of links - links, id_user = id
+  //   // array of causes - users_causes - id_user = id, id_cause = causes id
+  //   // array of skills - users_skills - id_user = id,  id_skill = skills id 
+  //   // array of responses - responses, id_user = id, id_opp = opportunities id, id_user = orgs id
+  //   // array of adminOf - roles - id_user_receiving = id, id_user_adding = id of org where capabilities = admin
+  //   // array of followingroles - id_user_receiving = id, id_user_adding = id of org where capabilities = admin
+  // });
 
   // ***** POST USER
-  describe('api/user POST new user', function() {
+  describe('api/users POST new user', function() {
 
     it('should add user to users table as type individual', function() {
       const testUser = testData.testIndividual;
 
       return chai.request(app)
-        .post('/api/user')
+        .post('/api/users')
         .send(testUser)
         .then(function(res) {
           expect(res.body.length).to.equal(1);
@@ -85,7 +86,7 @@ describe('user', function() {
 
         .then( () => {
           return chai.request(app)
-            .get('/api/user/list')
+            .get('/api/users/list')
             .then(function(res) {
               expect(res.body.length).to.equal(1);
               expect(res.body[0].user_type).to.equal('individual');
