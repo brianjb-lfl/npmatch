97c201fff93f81e0664adae9a9ac6a0ac5aa394c
bradcgarner <brad@bradgarner.com>
2017-12-11 10:47:59 -0500

    opps list fetch working

--- client/src/actions/opportunitiesList.js
+++ client/src/actions/opportunitiesList.js
@@ -1,6 +1,7 @@
 import 'whatwg-fetch';
 import { REACT_APP_BASE_URL } from '../config'
 import {SubmissionError} from 'redux-form';
+import  * as actionsDisplay from './display';
 
 // right now we have 1 'main' list of opportunities; we can have as many lists as we want, each following identical format
 // if we add lists, each one should have an action and reducer; each one can populate 1 key, like "main" does
@@ -12,3 +13,53 @@ export const loadOpportunitiesList = (array) => ({
 })
 
 // @@@@@@@@@@@@@@@ ASYNC @@@@@@@@@@@@@@@@@
+
+export const fetchOppsList = (searchCriteria, authToken) => dispatch => {
+  /* searchCriteria should be an object with following props.
+    values are priority, 1 being soonest for MVP
+    {
+      userId                    6 
+      organization              6
+      opportunityType  1
+      offer               2
+      title            1
+      narrative                 4
+      timestampStart      2
+      timestampEnd        2
+      locationCity            3
+      locationState           3
+      locationCountry         3
+      link:                     4
+      causes           1
+      responses                 5
+    }
+  */
+  
+  dispatch(actionsDisplay.changeDisplay('loading'));
+
+  const url = `${REACT_APP_BASE_URL}/api/opportunities/list`;
+  const headers = {
+    'content-type': 'application/json',
+    // "Authorization": `Bearer ${authToken}`, 
+  }; 
+
+  const init = { 
+    method: 'GET',
+    headers,
+    body: JSON.stringify(searchCriteria)
+  };
+  return fetch(url, init)    
+    .then(res=>{
+      return res.json();
+    })
+    .then(res=>{
+      console.log('response from opps list fetch',res)
+      dispatch(loadOpportunitiesList(res));
+      return dispatch(actionsDisplay.changeDisplay('loading'));
+      
+    })
+    .catch(error => {
+      console.log('error',error);
+      return dispatch(actionsDisplay.toggleModal(error));
+    })
+}
--- client/src/tests/opportunitiesList-action.test.js
+++ client/src/tests/opportunitiesList-action.test.js
@@ -1,4 +1,15 @@
-import * as actions from '../actions/opportunitiesList';
+import * as actionsOppsList from '../actions/opportunitiesList';
+import * as actionsDisplay from '../actions/display';
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+const middlewares = [ thunk ];
+const mockStore = configureStore(middlewares);
+// console.log('mockStore1',mockStore());
+
+// Initialize mockstore with empty state
+const initialState = {}
+const store = mockStore(initialState)
 
 describe('actions - opportunities list', () => {
 
@@ -36,7 +47,7 @@ describe('actions - opportunities list', () => {
       }
     ];
     const expectedAction = {
-      type: actions.LOAD_OPPORTUNITIES_LIST,
+      type: actionsOppsList.LOAD_OPPORTUNITIES_LIST,
       main: [
         {
           id: 1,
@@ -70,8 +81,44 @@ describe('actions - opportunities list', () => {
         }
       ]
     }
-    const result = actions.loadOpportunitiesList(arrayOfOpportunities);
+    const result = actionsOppsList.loadOpportunitiesList(arrayOfOpportunities);
     expect(result).toEqual(expectedAction)
   });
+
+  it('should call actions to fetch opportunities list from server', () => {
+    
+        const mockResponse = (status, statusText, response) => {
+          return new window.Response(response, {
+            status: status,
+            statusText: statusText,
+            headers: {
+              'Content-type': 'application/json'
+            }
+          });
+        };
+    
+        const expectedResponse = {}; // this will be the response of the mock call
+        const searchCriteria = {}; // doesn't matter for testing
+        const authToken = '';
+    
+        // cannot find docs on window.fetch. Does this intercept fetch in "this" window?
+        // i.e. when actions.fetchUsersList is called, does this intercept, because it is in the same "window"? 
+        window.fetch = jest.fn().mockImplementation(() =>
+          Promise.resolve(mockResponse(200, null, JSON.stringify(expectedResponse))));
+    
+        // console.log('store',store);
+          
+        return store.dispatch(actionsOppsList.fetchOppsList(searchCriteria, authToken))
+          .then(() => {
+            const expectedActions = store.getActions();
+            console.log('expectedActions',expectedActions)
+            expect(expectedActions.length).toBe(3);
+            expect.assertions(2);  // number of callback functions
+            expect(expectedActions).toContainEqual(
+              {type: actionsDisplay.CHANGE_DISPLAY, view: 'loading'},
+              {type: actionsOppsList.LOAD_OPPS_LIST, main: {} }
+            );
+          })
+      });
 
 })
