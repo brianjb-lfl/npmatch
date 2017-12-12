f4234e8a3c1d44b02f182c729f3e62c5da4946fe
bradcgarner <brad@bradgarner.com>
2017-12-12 12:59:25 -0500

    merge with Ben

--- client/src/store.js
+++ client/src/store.js
@@ -4,10 +4,5 @@ import thunk from 'redux-thunk';
 
 export const store = createStore (reducer, applyMiddleware(thunk));
 
-// console.log('reducer',typeof reducer);
-// console.log('createStore',typeof createStore);
-// console.log('applyMiddleware',typeof applyMiddleware);
-// console.log('thunk',typeof thunk);
-
 // for testing
-module.exports = {store, reducer, createStore, applyMiddleware, thunk};
+export {reducer, createStore, applyMiddleware, thunk};
