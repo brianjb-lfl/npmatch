21faa8d9fee5a0829af74cb193fb2ad84532c9f5
bradcgarner <brad@bradgarner.com>
2017-12-13 11:46:37 -0500

    fixed display reducer

--- client/src/reducers/display.js
+++ client/src/reducers/display.js
@@ -1,5 +1,5 @@
 import * as actions from '../actions/display'
-import {display as initialState} from './potentialStates'
+import {display as initialState} from './potential-states'
 
 // right now we have 1 'main' list of users; we can have as many lists as we want, each following identical format
 // if we add lists, each one should have an action and reducer; each one can populate 1 key, like "main" does
