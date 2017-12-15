146bdfdca54997dab8885f5198d75417ff7fe1f3
Benjamin Malin <benjaminamalin@gmail.com>
2017-12-15 13:36:32 -0500

    added styling for login, register, and landing pages, plus styling for preview components

--- client/src/components/opportunity-preview/opportunity-preview.js
+++ client/src/components/opportunity-preview/opportunity-preview.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import './opportunity-preview.css'
+
 export default class OpportunityPreview extends Component {
 
   render() {
     return (
-      <div>
+      <div className='opportunityPreview'>
         <h3 className='opportunityTitle'>{this.props.opportunity.title}</h3>
         <h4 className='requiredSkills'>{this.props.opportunity.requiredSkills}</h4>
         <p className='timeframe'>{this.props.opportunity.requiredSkills}</p>
--- client/src/components/register-page/register-page.js
+++ client/src/components/register-page/register-page.js
@@ -5,71 +5,75 @@ import { Field, reduxForm } from 'redux-form';
 import { display } from '../../reducers/potential-states';
 import * as userActions from '../../actions/user';
 
+import './register-page.css';
+
 export class RegisterPage extends Component {
 
   handleTypeChange(userType) {
     this.props.dispatch(userActions.setFormType(userType))
   }
-  
+
   handleSubmitButton(input) {
     this.props.dispatch(userActions.registerUser(input))
       .then(() => this.props.history.push('/'))
   }
 
-  indivFormBody = 
+  indivFormBody =
     <div>
+      <label className='inputLabel' htmlFor={'firstName'}>FirstName</label>
       <Field className='firstNameInput' name='firstName' id='firstName'
         component='input' type='text' placeholder='First Name' required
       />
-      <label className='inputLabel' htmlFor={'firstName'}>FirstName</label>
 
+      <label className='inputLabel' htmlFor={'lastName'}>Last Name</label>
       <Field className='lastNameInput' name='lastName' id='lastName'
         component='input' type='text' placeholder='Last Name' required
       />
-      <label className='inputLabel' htmlFor={'lastName'}>Last Name</label>
     </div>;
-  
-  orgFormBody = 
+
+  orgFormBody =
     <div>
+      <label className='inputLabel' htmlFor={'organization'}>Organization</label>
       <Field className='organizationInput' name='organization' id='organization'
         component='input' type='text' placeholder='Organization Name' required
       />
-      <label className='inputLabel' htmlFor={'organization'}>Organization</label>
     </div>;
 
   render() {
-    this.formBody = this.props.user.formType === 'individual' ? this.indivFormBody : this.orgFormBody ;
-    
+    this.formBody = this.props.user.formType === 'individual' ? this.indivFormBody : this.orgFormBody;
+
     return (
       <main>
         <form className='registerForm'
           onSubmit={this.props.handleSubmit((values) => this.handleSubmitButton(values))}
         >
+          <label className='inputLabel' htmlFor={'userTypeI'}>Individual</label>
           <Field className='userTypeInput' name='userType' id='userTypeI'
             component='input' type='radio' value='individual' onChange={() => this.handleTypeChange('individual')}
           />
-          <label className='inputLabel' htmlFor={'userTypeI'}>Individual</label>
+
+          <label className='inputLabel' htmlFor={'userTypeO'}>Organization</label>
           <Field className='userTypeInput' name='userType' id='userTypeO'
             component='input' type='radio' value='organization' onChange={() => this.handleTypeChange('organization')}
           />
-          <label className='inputLabel' htmlFor={'userTypeI'}>Organization</label>
 
           {this.formBody}
 
+          <label className='inputLabel' htmlFor={'username'}>Username</label>
           <Field className='usernameInput' name='username' id='username'
             component='input' type='text' placeholder='Email Address' required
           />
-          <label className='inputLabel' htmlFor={'username'}>Username</label>
 
+          <label className='inputLabel' htmlFor={'password'}>Password</label>
           <Field className='passwordInput' name='password' id='password'
             component='input' type='password' placeholder='Password' required
           />
-          <label className='inputLabel' htmlFor={'password'}>Password</label>
 
+          <label className='inputLabel' htmlFor={'password2'}>Confirm Password</label>
           <Field className='passwordInput' name='password2' id='password2'
             component='input' type='password' placeholder='Confirm Password' required
           />
-          <label className='inputLabel' htmlFor={'password2'}>Confirm Password</label>
+
           <button type='submit'>Sign Up</button>
         </form>
       </main>
