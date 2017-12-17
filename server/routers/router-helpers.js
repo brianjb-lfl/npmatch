7a562647534f45db8b66fec083e41b002d7ca859
Brian Besaw <bjbesaw@outlook.com>
2017-12-17 01:24:36 -0500

    Added mechanism to filter user GET by req.query terms if passed. Also added helper function to convert an objects fields from camelCase to snake_case or v/v.

--- server/routers/router-helpers.js
+++ server/routers/router-helpers.js
@@ -99,4 +99,97 @@ epHelp.buildOppCausesArr = function(oppId, inCausesArr) {
     });
 };
 
+// const ccToSnake = {
+//   userType: 'user_type',
+//   locationCity: 'location_city',
+//   locationState: 'location_state',
+//   locationCountry: 'location_country',
+//   firstName: 'first_name',
+//   lastName: 'last_name',
+//   opportunityType: 'opportunity_type',
+//   userId: 'id_user',
+//   idCause: 'id_cause',
+//   idOpp: 'id_opp',
+//   idSkill: 'id_skill',
+//   idUserAdding: 'id_user_adding',
+//   idUserReceiving: 'id_user_receiving',
+//   linkType: 'link_type',
+//   linkUrl: 'link_url',
+//   responseStatus: 'response_status',
+//   timestampCreated: 'timestamp_created',
+//   timestampStart: 'timestamp_start',
+//   timestampEnd: 'timestamp_end',
+//   timestampStatusChange: 'timestamp_status_change'
+// };
+
+const snakeToCC = {
+  user_type: 'userType',
+  location_city: 'locationCity',
+  location_state: 'locationState',
+  location_country: 'locationCountry',
+  first_name: 'firstName',
+  last_name: 'lastName',
+  opportunity_type: 'opportunityType',
+  id_user: 'userId',
+  id_cause: 'idCause',
+  id_opp: 'idOpp',
+  id_skill: 'idSkill',
+  id_user_adding: 'idUserAdding',
+  id_user_receiving: 'idUserReceiving',
+  link_type: 'linkType',
+  link_url: 'linkUrl',
+  response_status: 'responseStatus',
+  timestamp_created: 'timestampCreated',
+  timestamp_start: 'timestampStart',
+  timestamp_end: 'timestampEnd',
+  timestamp_status_change: 'timestampStatusChange'
+};
+
+epHelp.convertCase = function(caseObj, mode) {
+
+  let convTbl = {};
+
+  const ccToSnake = {
+    userType: 'user_type',
+    locationCity: 'location_city',
+    locationState: 'location_state',
+    locationCountry: 'location_country',
+    firstName: 'first_name',
+    lastName: 'last_name',
+    opportunityType: 'opportunity_type',
+    userId: 'id_user',
+    idCause: 'id_cause',
+    idOpp: 'id_opp',
+    idSkill: 'id_skill',
+    idUserAdding: 'id_user_adding',
+    idUserReceiving: 'id_user_receiving',
+    linkType: 'link_type',
+    linkUrl: 'link_url',
+    responseStatus: 'response_status',
+    timestampCreated: 'timestamp_created',
+    timestampStart: 'timestamp_start',
+    timestampEnd: 'timestamp_end',
+    timestampStatusChange: 'timestamp_status_change'
+  };
+
+  if(mode === 'ccToSnake') {
+    convTbl = ccToSnake;
+  }
+  else {
+    convTbl = snakeToCC;
+  }
+
+  Object.keys(caseObj).forEach ( key => {
+    if(convTbl[key]) {
+      caseObj = Object.assign( {}, caseObj, {
+        [convTbl[key]]: caseObj[key]
+      });
+      delete caseObj[key];
+    }
+  })
+
+  return caseObj
+
+}
+
 module.exports = { epHelp };
