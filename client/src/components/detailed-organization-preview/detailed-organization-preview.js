86f2006d0545ae53590769af109e5c277dae1165
Benjamin Malin <benjaminamalin@gmail.com>
2017-12-18 13:40:17 -0500

    expanding out tests - opportunity preview test 57% done

--- client/src/components/detailed-organization-preview/detailed-organization-preview.js
+++ client/src/components/detailed-organization-preview/detailed-organization-preview.js
@@ -17,9 +17,9 @@ export class DetailedOrganizationPreview extends Component {
         <img className='logo' src={this.props.user.logo} alt={`${this.props.user.organization} logo`}></img>
         <h3 className='name'>{this.props.user.name}</h3>
         <p className='bio'>{this.props.user.bio}</p>
-        <p className='causes'>{this.props.user.causes.join(', ')}</p>
-        <p className='skillsSought'>{this.props.user.skillsSought.join(', ')}</p>
-        <p className='upcomingProjects'>{this.props.user.upcomingProjects.join(', ')}</p>
+        {/* <p className='causes'>{this.props.user.causes.join(', ')}</p> */}
+        {/* <p className='skillsSought'>{this.props.user.skillsSought.join(', ')}</p> */}
+        {/* <p className='upcomingProjects'>{this.props.user.upcomingProjects.join(', ')}</p> */}
       </div>
     )
   }
