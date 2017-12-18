8cc95f71589864b9e42201e2a82094996435b637
Benjamin Malin <benjaminamalin@gmail.com>
2017-12-18 12:26:30 -0500

    updated tests and components to fix errors

--- client/src/components/landing-page/landing-page.test.js
+++ client/src/components/landing-page/landing-page.test.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
-import LandingPage from './landing-page';
+import { mapStateToProps } from './landing-page';
+import { LandingPage } from './landing-page';
+import { OrganizationPreview } from '../organization-preview/organization-preview';
+
+const mockStore = configureStore(middle)
 
 describe('Landing Page component display functionality', () => {
+  const spy = jest.fn();
+  const usersList = [{
+    main: {
+      id: 1,
+    }
+  }];
   it('Smoke test - component should render', () => {
-    shallow(<LandingPage />);
+    shallow(<LandingPage dispatch={spy} />);
   });
+  it('Should display organizations', () => {
+    const wrapper = shallow(<LandingPage dispatch={spy} usersList={usersList} />)
+    expect(wrapper.containsMatchingElement(
+      <OrganizationPreview />
+    ));
+  });
+  // it('Should map state to props', () => {
+  //   const initialState = { display: 'landingPage', usersList: usersList };
+  //   const expectedProps = { display: 'landingPage', usersList: [{
+  //     main: {
+  //       id: 1,
+  //     }
+  //   }]};
+  //   const mockState = mapStateToProps(initialState);
+  //   expect(mockState).toEqual(expectedProps);
+  // })
 });
