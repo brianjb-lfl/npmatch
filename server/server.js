092908158aa9256a42ce5df99f521be00181abda
Brian Besaw <bjbesaw@outlook.com>
2017-12-19 11:23:49 -0500

    troubleshooting

--- server/server.js
+++ server/server.js
@@ -25,8 +25,10 @@ app.use(
 );
 
 
-app.use(express.static('../client/public')); 
-app.get('/', (req, res) => { res.sendFile('../client/public/index.html'); });
+app.use(express.static(path.join(__dirname, '..', 'client/public/index.html'))); 
+app.get('/', (req, res) => { 
+  res.sendFile(path.join(__dirname, '..', 'client/public/index.html'));
+});
 
 app.use('/api/users', userRouter);
 app.use('/api/orgs', orgRouter);
